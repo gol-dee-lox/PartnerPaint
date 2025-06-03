f086c224d0a13da2b5ce1aebd67df71c5f35e1cc
gol-dee-lox <randy@therealrandy.com>
2025-06-02 22:25:33 -0400

    Cors file adjustment

--- src/main/java/com/goldeelox/gridserver/config/CorsConfig.java
+++ src/main/java/com/goldeelox/gridserver/config/CorsConfig.java
@@ -2,21 +2,23 @@ package com.goldeelox.gridserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
-public class CorsConfig { 
+public class CorsConfig {
+
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://partnerpaint.goldeelox.com")
-                        .allowedMethods("*")
-                        .allowedHeaders("*");
-            }
-        };
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("https://partnerpaint.goldeelox.com");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);  // Optional depending on whether you're sending cookies
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
